3a02657cfc6fcd19b8d234fbead491b47fcc9c64
RAHUL KHALLAR <khallarrahul@gmail.com>
2023-03-13 15:48:59 -0700

    adds arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+//first test
+let num1 = 2;
+let num2 = 31;
+let multiply = num1*num2
+console.log(multiply);
+
+//second test
+let random = Math.floor(Math.random()*10)+1;
+console.log(random);
+
+//third test
+let num3 = 16;
+let num4 = 6;
+let mod = (num3 %num4);
+console.log(mod);
+
+//fourth test
+let max = Math.max(15,16,17,18,19,20);
+console.log(max);
